5b5e30daa3f8281e6ee70809803f04490d993a8e
Elena Ponomareva <eponomar9@gmail.com>
2025-10-24 21:42:43 +0300

    fix lint (ошибки были в заданиях с лекции)

--- lesson7/build.gradle.kts
+++ lesson7/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
-    id("buildlogic.kotlin-library-conventions-no-detekt")
-}
+        id("buildlogic.kotlin-library-conventions")
+    }
 dependencies {
-    testImplementation(kotlin("test"))
-}
+        testImplementation(kotlin("test"))
+    }
+
+        plugins { buildlogic.`kotlin-common-conventions-no-detekt` }
+dependencies { testImplementation(kotlin("test")) }
