7a2b6a7c67019010efecf6f3ce537059aca09885
Elena Ponomareva <eponomar9@gmail.com>
2025-12-21 17:51:48 +0300

    сделала дз по 11 уроку

--- lesson11/src/main/kotlin/homework/parallelTransform.kt
+++ lesson11/src/main/kotlin/homework/parallelTransform.kt
@@ -1,5 +1,9 @@
 package homework
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+
 /**
  * Задание: Параллельное преобразование элементов списка с использованием async.
  *
@@ -12,6 +16,11 @@ package homework
 suspend fun <T, R> parallelTransform(
     items: List<T>,
     transform: suspend (T) -> R
-): List<R> {
-    TODO("Реализуйте параллельное преобразование с использованием async/await")
+): List<R> = coroutineScope {
+    val res = items.map { item ->
+        async {
+            transform(item)
+        }
+    }
+    res.awaitAll()
 }
